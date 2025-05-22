064a68300dccdb83c7af54922d24a1344a76fb5f
Deivid21 <david.parra.ignacio@gmail.com>
2025-05-22 02:20:28 -0400

    sm6225-common: Init changes for Evolution-X
    Change-Id: Ie395a951cfe99a80f32f824866c16db7fa6978d5

--- common.mk
+++ common.mk
@@ -149,6 +149,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti.vendor # Needed by CNE app
 
+# GApps
+WITH_GMS := true
+TARGET_USES_MINI_GAPPS := true
+
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@2.1-impl-qti \
